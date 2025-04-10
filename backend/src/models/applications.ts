83d10822315e2a731be3f2d441b0366dd612717e
Joe Cullin <joe@joecullin.com>
2025-04-09 20:34:27 -0400

    try/catch for reading data

--- backend/src/models/applications.ts
+++ backend/src/models/applications.ts
@@ -6,11 +6,24 @@ const dataFile = "applications.json";
 const applicationsDataFile = [process.env.LOCAL_FILESYSTEM_DATA_PATH, dataFile].join("/");
 
 export const getAll = async (): Promise<Application[]> => {
-    const fileData = readFileSync(applicationsDataFile, "utf-8");
-    const applications = JSON.parse(fileData);
-    return applications;
+    try {
+        const fileData = readFileSync(applicationsDataFile, "utf-8");
+        const applications = JSON.parse(fileData);
+        return applications;
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`error reading applications data from disk: ${error.stack}`);
+        }
+        return [];
+    }
 };
 
 export const saveAll = async (applicationArray: Application[]): Promise<void> => {
-    await writeFileSync(applicationsDataFile, JSON.stringify(applicationArray, null, 4), "utf-8");
+    try {
+        await writeFileSync(applicationsDataFile, JSON.stringify(applicationArray, null, 4), "utf-8");
+    } catch (error) {
+        if (error instanceof Error) {
+            console.error(`error writing applications data to disk: ${error.stack}`);
+        }
+    }
 };
